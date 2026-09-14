632d0b7b20f37415b69bf34a0a6c9f0c43ee5012
Stiff-Rock <yaguitor@gmail.com>
2026-09-14 22:03:48 +0200

    Pro CV

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-addEventListener("DOMContentLoaded", _ => {
-  let langCombo = document.getElementById("lenguage-combobox");
-  let printBtn = document.getElementById("print-btn");
-  let portfolioChckbx = document.getElementById("portfolio-chckbx");
+let specificCss;
+let typeCombo;
+let langCombo;
+let printBtn;
+let portfolioChckbx;
+let cvContainer;
 
-  if (!langCombo || !printBtn || !portfolioChckbx) return;
+let currentCv;
+
+const cvDict = {
+  "cool": {
+    "html": "./src/cool-cv.html",
+    "css": "./css/cool-cv.css"
+  },
+  "pro": {
+    "html": "./src/pro-cv.html",
+    "css": "./css/pro-cv.css"
+  }
+}
+
+addEventListener("DOMContentLoaded", async _ => {
+  specificCss = document.getElementById("specific-css");
+  typeCombo = document.getElementById("type-combobox");
+  langCombo = document.getElementById("lenguage-combobox");
+  printBtn = document.getElementById("print-btn");
+  portfolioChckbx = document.getElementById("portfolio-chckbx");
+  cvContainer = document.getElementById("cv-container");
+
+  currentCv = typeCombo.value;
+
+  typeCombo.addEventListener("change", async (event) => {
+    currentCv = event.target.value;
+    await loadCv();
+    await loadTranslationSheet(langCombo.value);
+  });
 
   langCombo.addEventListener("change", (event) => {
     loadTranslationSheet(event.target.value);
@@ -18,11 +47,30 @@ addEventListener("DOMContentLoaded", _ => {
     document.getElementById('portfolio-link').style.display = display;
   });
 
-  loadTranslationSheet(langCombo.value);
+  await loadCv();
+  await loadTranslationSheet(langCombo.value);
 })
 
+async function loadCv() {
+  const data = cvDict[currentCv];
+
+  specificCss.href = data.css;
+
+  try {
+    const res = await fetch(data.html)
+    if (!res.ok) {
+      throw new Error(`Error loading file: ${response.status}`);
+    }
+
+    cvContainer.innerHTML = await res.text();
+  } catch (error) {
+    console.error('Error loading CV content:', error);
+    cvContainer.innerHTML = '<p>Error loading CV content.</p>';
+  }
+}
+
 async function loadTranslationSheet(lang) {
-  document.title = `CV - Yago Pernas (${lang})`;
+  document.title = `CV ${(currentCv == 'cool' ? 'Tech' : 'Pro')} - Yago Pernas (${lang})`;
 
   try {
     const response = await fetch(`./translations/${lang}.json`)
@@ -42,13 +90,17 @@ async function loadTranslationSheet(lang) {
 function change_lang(translation) {
   document.querySelectorAll('[data-lang-key]').forEach(element => {
     const key = element.getAttribute('data-lang-key');
-    const translatedText = translation[key];
+    let translatedText = translation[key];
     if (!translatedText) return;
 
     if (element.hasAttribute('data-tag')) {
-      element.setAttribute('data-tag', translation[key])
+      element.setAttribute('data-tag', translatedText);
     } else {
-      element.textContent = translation[key];
+      if (typeof currentCv !== 'undefined' && currentCv === 'pro' && translatedText.includes("■")) {
+        translatedText = translatedText.replace("■", "").trimStart();
+      }
+
+      element.textContent = translatedText;
     }
   });
 }
